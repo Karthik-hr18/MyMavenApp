e90aeb21d32b4abeda8845b56851091bd71a472f
Karthik-hr18 <karthikhrvidyanidhi676@gmail.com>
2026-02-23 09:45:36 +0530

    Updated

---
+++ src/main/java/com/example/MainClass.java
@@ -0,0 +1,9 @@
+package com.example;
+public class MainClass {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println(c);
+    }
+}
